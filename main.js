1086212fd38da63d1704bd277a8d61e11015b234
FELIPE VIEIRA DOS SANTOS <24122377@dsn127m1249387.cfp127aluno.local>
2025-08-07 15:43:32 -0300

    frontend v.1

--- main.js
+++ main.js
@@ -1,51 +1,48 @@
 'use strict'
 
-const galeria = document.getElementById('galeria')
+const galeriaSlider = document.getElementById('galeria-slide')
 
-async function chamarFotos(){
-    const response = await fetch("http://localhost:3000/fotos")
-    // const response = await fetch(BASE_API)
-    const dados = await response.json()
-
-    if(dados){
-        return dados
-    }else{
-        return console.error("Erro ao acessar API");
-    }
+async function chamarFotos() {
+  const response = await fetch("http://localhost:3000/fotos")
+  const dados = await response.json()
+  return dados || console.error("Erro ao acessar API")
 }
 
-async function pegarFotos(){
-   const fotos = await chamarFotos()
-    return fotos
-    // console.log(fotos);
+async function exibirFotos() {
+  const fotos = await chamarFotos()
 
+  fotos.forEach(foto => {
+    const imagem = document.createElement('img')
+    imagem.src = foto.imagem
+    imagem.alt = foto.legenda
+
+    const legenda = document.createElement('h2')
+    legenda.textContent = foto.legenda
+
+    const data = document.createElement('p')
+    data.textContent = `Data: ${foto.data}`
+
+    const container = document.createElement('div')
+    container.classList.add('galeria-item')
+
+    container.appendChild(imagem)
+    container.appendChild(legenda)
+    container.appendChild(data)
+
+    galeriaSlider.appendChild(container)
+  })
+
+  iniciarSlide(fotos.length) // Ativa o slide automático
 }
 
-async function exibirFotos(){
-    const fotos = await pegarFotos()
+let slideAtual = 0
 
-    fotos.forEach(foto => {
-        const imagem = document.createElement('img')
-        imagem.src = foto.imagem
-        imagem.alt = foto.legenda
-
-        const legenda = document.createElement('p')
-        legenda.textContent = foto.legenda
-
-        const data = document.createElement('p')
-        data.textContent = `Data: ${foto.data}`
-
-        const container = document.createElement('div')
-
-        container.appendChild(imagem)
-        container.appendChild(legenda)
-        container.appendChild(data)
-
-        galeria.appendChild(container)
-
-        
-    });
+function iniciarSlide(qtdSlides) {
+  setInterval(() => {
+    slideAtual = (slideAtual + 1) % qtdSlides
+    const larguraSlide = document.getElementById('galeria').offsetWidth
+    document.getElementById('galeria-slide').style.transform = `translateX(-${slideAtual * larguraSlide}px)`
+  }, 4000) // Muda de slide a cada 3 segundos
 }
+
 exibirFotos()
-
-
